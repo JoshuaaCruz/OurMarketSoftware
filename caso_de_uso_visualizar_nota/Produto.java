d0dfcc66cd665128256b6788f033c6c24c080140
JoshuaaCruz <joshua.cruz0520@gmail.com>
2026-05-07 19:28:23 -0300

    Feat: add caso uso visualizar nota produto e vendedor

---
+++ caso_de_uso_visualizar_nota/Produto.java
@@ -0,0 +1,102 @@
+import java.util.List;
+
+
+public class Produto {
+    private String nome;
+    private String descricao;
+    private double precoBase;
+    private Cliente vendedor;
+    private Categoria categoria;
+
+    private double nota;
+    private List<String> comentarios;
+
+    
+    public Produto(String nome, String descricao, double precoBase){
+        this.nome=nome;
+        this.descricao=descricao;
+        this.precoBase=precoBase;
+    }
+    
+    
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @return the descricao
+     */
+    public String getDescricao() {
+        return descricao;
+    }
+
+    /**
+     * @return the precoBase
+     */
+    public double getPrecoBase() {
+        return precoBase;
+    }
+
+    /**
+     * @return the vendedor
+     */
+    public Cliente getVendedor() {
+        return vendedor;
+    }
+
+    /**
+     * @return the categoria
+     */
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    /**
+     * @param categoria the categoria to set
+     */
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    /**
+     * @param vendedor the vendedor to set
+     */
+    public void setVendedor(Cliente vendedor) {
+        this.vendedor = vendedor;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public void setPrecoBase(double precoBase) {
+        this.precoBase = precoBase;
+    }
+
+    public void setNota(double nota) {
+        this.nota = nota;
+    }
+
+    public void setComentarios(List<String> comentarios) {
+        this.comentarios = comentarios;
+    }
+
+    public void addComentario(String comentario) {
+        this.comentarios.add(comentario);
+    }
+
+    /**
+     * @return the nota
+     */
+    public double getNota() {
+        return nota;
+    }
+
+}
